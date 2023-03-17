279b08b0ae580ef74f56c4391cc13d2160d07c9a
Guy Menahem <guy@komodor.io>
2023-03-17 15:53:04 +0200

    feat: add custom msg

--- kubernetes/ready-live-probes/probes-example-simple-app/app.py
+++ kubernetes/ready-live-probes/probes-example-simple-app/app.py
@@ -11,15 +11,35 @@ app = Flask("integration-application")
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(name)s - %(message)s')
 
 liveness_env_should_fail = "FAIL_LIVE"
+liveness_fail_msg = "LIVE_FAIL_MSG"
 readiness_env_should_fail = "FAIL_READY"
+readiness_fail_msg = "READY_FAIL_MSG"
 
 
 @app.route('/live')
 def live_check():
     should_fail_live = strtobool(os.environ.get(liveness_env_should_fail, False))
-    logging.info(f'should fail in live probe: {should_fail_live}')
     if should_fail_live:
-        return make_response("Internal Server Error", 500)
+        msg = os.environ.get(liveness_fail_msg, "Internal Server Error")
+        logging.error(msg + " - retrying")
+        time.sleep(1)
+        logging.error(msg + " - retrying")
+        time.sleep(1)
+        logging.error(msg + " - failing probe")
+        return make_response(msg, 500)
+    return make_response("OK", 200)
+
+@app.route('/ready')
+def ready_check():
+    should_fail_ready = strtobool(os.environ.get(readiness_env_should_fail, False))
+    if should_fail_ready:
+        msg = os.environ.get(readiness_fail_msg, "Internal Server Error")
+        logging.error(msg + " - retrying")
+        time.sleep(1)
+        logging.error(msg + " - retrying")
+        time.sleep(1)
+        logging.error(msg + " - failing probe")
+        return make_response(msg, 500)
     return make_response("OK", 200)
 
 @app.route('/fail_live')
@@ -31,15 +51,6 @@ def fail_live():
 @app.route('/start_live')
 def start_live():
     os.environ[liveness_env_should_fail] = "False"
-    return make_response("OK", 200)
-
-
-@app.route('/ready')
-def ready_check():
-    should_fail_ready = strtobool(os.environ.get(readiness_env_should_fail, False))
-    logging.info(f'should fail in ready probe: {should_fail_ready}')
-    if should_fail_ready:
-        return make_response("Internal Server Error", 500)
     return make_response("OK", 200)
 
 
